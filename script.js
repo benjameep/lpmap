53ab1ebb61bb82689840a35faa91acf164193de0
benjameep <14208829+benjameep@users.noreply.github.com>
2024-09-20 14:24:11 -0600

    Fix safari issue hopefully

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ function use(current) {
   }
 }
 
+$('.desk').attr({
+  'fill': 'black',
+  'fill-opacity': 0.2
+})
+
+$('circle.light').attr({
+  'fill': 'url(#g)',
+  'r': 5.5,
+})
+
+$('text.light').attr({
+  'fill': 'white',
+  'text-anchor': 'middle',
+  'alignment-baseline': 'middle',
+  'font-size': 22,
+  'font-family': 'monospace',
+  'font-weight': 900,
+})
+
+$('.wall').attr({
+  'fill': '#373130',
+  'stroke': '#222222',
+})
+
 use('alimentations')
