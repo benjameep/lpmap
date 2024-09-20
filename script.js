62a09a752bc3fb26964d8f453afad26695c85fc5
benjameep <14208829+benjameep@users.noreply.github.com>
2024-09-20 17:16:56 -0600

    remove jquery and inline the svg styling

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
 function use(current) {
   const options = ['vent','alimentations','delivery','computer']
   for (const opt of options) {
-    $(`button.${opt}`).toggleClass('active', opt === current)
-    $(`.light.${opt}`).attr('display', opt === current ? 'block' : 'none')
+    document.querySelector(`button.${opt}`).classList.toggle('active', opt === current)
+    document.querySelectorAll(`circle.${opt}, text.${opt}`).forEach(light => {
+      if (opt !== current) {
+        light.setAttribute('display', 'none')
+      } else {
+        light.removeAttribute('display')
+      }
+    })
   }
 }
-
-$('.desk').attr({
-  'fill': 'black',
-  'fill-opacity': 0.2
-})
-
-$('circle.light').attr({
-  'fill': 'url(#g)',
-  'r': 5.5,
-})
-
-$('text.light').attr({
-  'fill': 'white',
-  'text-anchor': 'middle',
-  'alignment-baseline': 'middle',
-  'font-size': 22,
-  'font-family': 'monospace',
-  'font-weight': 900,
-})
-
-$('.wall').attr({
-  'fill': '#373130',
-  'stroke': '#222222',
-})
-
-use('alimentations')
